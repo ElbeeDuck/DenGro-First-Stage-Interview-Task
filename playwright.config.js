b60185dcedc0379c3b5446f94060b2a487e867cc
ElbeeDuck <elbee@duck.com>
2025-02-06 22:15:15 +0000

    Adding firefox and webkit to config file

--- playwright.config.js
+++
@@ -1,7 +0,0 @@
-// playwright.config.js
-module.exports = {
-    testDir: './tests', // The directory where your tests are located
-    use: {
-      headless: false, // Run tests with a visible browser window
-    },
-  };
